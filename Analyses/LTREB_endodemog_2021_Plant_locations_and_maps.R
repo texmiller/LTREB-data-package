e82c765e0cbe97dc4eb57d805b50e0002a2d3246
joshuacfowler <jcf3@rice.edu>
2021-05-27 18:56:02 -0500

    trying to make maps with a and b posts, with both options for orientation of posts.

--- Analyses/LTREB_endodemog_2021_Plant_locations_and_maps.R
+++ Analyses/LTREB_endodemog_2021_Plant_locations_and_maps.R
@@ -25,3 +25,45 @@ LTREB_2020_surviving <- LTREB_full %>%
 
 write_csv(LTREB_2020_surviving, path = "~/Documents/R_projects/Grass-Endophyte-Stochastic-Demography/LTREB_2020_surviving.csv")
 
+
+
+# Get x and y distances from the a and b corner posts
+# we calculate the area based on the perimeter; area = sqrt(s(s-a)(s-b)(s-c)) where s is half the perimeter
+# Then we get the height (y value) from the area; h = 2*A/b
+# Then we can get the x value as one side of a right triangle with the base and one side (x = sqrt(b^2 - y^2))
+dist_between_posts <- 3
+LTREB_xy <- LTREB_2020_surviving %>% 
+  mutate(triangle_perimeter = dist_a + dist_b + dist_between_posts,
+         s = triangle_perimeter*.5,
+         triangle_area = sqrt(s*(s-dist_a)*(s-dist_b)*(s-dist_between_posts)),
+         y = 2*triangle_area/dist_between_posts,
+         x = sqrt(dist_a^2-y^2))
+
+
+maps_aorigin <- ggplot(data = subset(LTREB_xy, plot_fixed == 1))+
+  geom_text(aes(x = x, y = y, label = id)) +
+  geom_text(aes(x = 0, y = 0, label = "A"), lwd = 4)+
+  geom_text(aes(x = dist_between_posts, y = 0, label = "B"), lwd = 4)+
+  lims(x = c(0,dist_between_posts), y = c(0, dist_between_posts)) +
+  theme_classic()
+maps_aorigin
+
+maps_aright <- ggplot(data = subset(LTREB_xy, plot_fixed == 1))+
+  geom_text(aes(x = dist_between_posts-x, y = dist_between_posts-y, label = id)) +
+  geom_text(aes(x = dist_between_posts, y = dist_between_posts, label = "A"), lwd = 4)+
+  geom_text(aes(x = 0, y = dist_between_posts, label = "B"), lwd = 4)+
+  lims(x = c(0,dist_between_posts), y = c(0, dist_between_posts)) +
+  theme_classic()
+maps_aright
+
+maps_aleft <- ggplot(data = subset(LTREB_xy, plot_fixed == 1))+
+  geom_text(aes(x = dist_between_posts-x, y = dist_between_posts-y, label = id)) +
+  geom_text(aes(x = dist_between_posts, y = dist_between_posts, label = "A"), lwd = 4)+
+  geom_text(aes(x = 0, y = dist_between_posts, label = "B"), lwd = 4)+
+  scale_x_reverse(c(dist_between_posts, 0))+
+  lims(y = c(0, dist_between_posts)) +
+  theme_classic()
+maps_aleft
+
+
+
