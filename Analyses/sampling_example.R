4cef50971e9074620d8ef596e773982364b3d48f
Tom Miller <tom.miller@rice.edu>
2022-10-07 12:57:03 -0500

    Create sampling_example.R

---
+++ Analyses/sampling_example.R
@@ -0,0 +1,13 @@
+
+lambda<-rnorm(15,1,0.15)
+toptwo <- lambda%in%sort(lambda)[c(1,15)]
+topfour <- lambda%in%sort(lambda)[c(1:2,14:15)]
+topsix <- lambda%in%sort(lambda)[c(1:3,13:15)]
+
+par(mfrow=c(4,1),mar=c(2,4,0,0))
+hist(lambda,xlim=c(0.7,1.3),breaks=10,main=" ",ylim=c(0,4),col="darkgray")
+hist(lambda[topsix],xlim=c(0.7,1.3),breaks=10,main=" ",ylim=c(0,4),col="forestgreen")
+hist(lambda[topfour],xlim=c(0.7,1.3),breaks=10,main=" ",ylim=c(0,4),col="cornflowerblue")
+hist(lambda[toptwo],xlim=c(0.7,1.3),breaks=10,main=" ",ylim=c(0,4),col="tomato")
+
+sd(lambda);sd(lambda[topsix]);sd(lambda[topfour]);sd(lambda[toptwo])
